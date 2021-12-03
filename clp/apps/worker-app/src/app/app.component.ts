8817e5e4bf27d37d5fc9f15025e6c14078ebd453
Andrey Varlamov <andrey.varlamov@juriba.com>
2021-12-03 12:57:17 +0500

    CLP init project with NX

---
+++ clp/apps/worker-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'clp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'worker-app';
+}
